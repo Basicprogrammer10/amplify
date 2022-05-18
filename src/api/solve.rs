512c1c6663ec6b5fbc92261d4a803940a0ab28df
Connor Slade <connor@connorcode.com>
2022-05-18 18:24:39 -0400

    🧢 Run user code!

--- src/api/solve.rs
+++ src/api/solve.rs
@@ -1,14 +1,26 @@
+use std::io::Write;
 use std::process::{Command, Stdio};
 use std::string::String;
 use std::time::Instant;
 
 use afire::{Content, Method, Response, Server};
-use serde_json::json;
+use serde_json::{from_str, json, Value};
+use tempfile;
 
 use crate::{App, Arc};
 
 pub fn attatch(server: &mut Server, app: Arc<App>) {
-    server.route(Method::GET, "/api/solve", move |_req| {
+    server.route(Method::POST, "/api/solve", move |req| {
+        let body = from_str::<Value>(&String::from_utf8_lossy(&req.body)).unwrap();
+        let code = body
+            .get("code")
+            .expect("No code defined")
+            .as_str()
+            .expect("Code is in a string");
+
+        let mut code_file = tempfile::NamedTempFile::new_in("data/tmp").unwrap();
+        code_file.write_all(code.as_bytes()).unwrap();
+
         let time = Instant::now();
         let run = Command::new(&app.cfg.docker_command)
             .args([
@@ -24,6 +36,8 @@ pub fn attatch(server: &mut Server, app: Arc<App>) {
                 "256m",
                 "--pids-limit",
                 "512",
+                "-v",
+                &format!("{}:/runner/src/main.rs", code_file.path().to_string_lossy()),
                 "-e",
                 &format!("TIMEOUT={}", &app.cfg.docker_timeout),
                 &app.cfg.docker_image,
@@ -34,11 +48,12 @@ pub fn attatch(server: &mut Server, app: Arc<App>) {
             .unwrap()
             .wait_with_output()
             .unwrap();
+        let time = time.elapsed().as_millis() as u64;
 
         Response::new()
             .text(json!(
                 {
-                    "time": time.elapsed().as_millis() as u64,
+                    "time": time,
                     "status": run.status.code().unwrap_or_default(),
                     "stdout": String::from_utf8_lossy(&run.stdout),
                     "stderr": String::from_utf8_lossy(&run.stderr)
