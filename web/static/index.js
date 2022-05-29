11d031329ed7c9c6624b371d9e9bd1b092bcaf46
Connor Slade <connor@connorcode.com>
2022-05-29 13:04:10 -0400

    🔺 UI progress

--- web/static/index.js
+++ web/static/index.js
@@ -11,6 +11,20 @@ const LANG_IDENTIFIERS = {
   python: "ace/mode/python",
   rust: "ace/mode/rust",
 };
+const COMPLETE_MESSAGES = [
+  "🎊 Congratulations! You completed this problem!",
+  "✨ very beans!!! You got it!",
+  "🧊 cool beans,,, You got it!",
+  "🥭 go go mango,,, You got it!",
+];
+const FAIL_MESSAGES = [
+  "hm, not quite",
+  "not quite, try again",
+  "*incorrect*",
+  "keep trying, i believe in you",
+  "never give up",
+  "you're getting close, i can feel it",
+];
 
 async function getSession() {
   let info = await (
@@ -47,10 +61,23 @@ async function run(lang, prob) {
 
   stdout.forEach((d) => {
     if (d.added) stdoutDiff += "<" + d.value.replace("\n", "\n<");
-    else if (d.removed) stdoutDiff += ">" + d.value.replace("\n", "\n<");
+    else if (d.removed) stdoutDiff += ">" + d.value.split("\n").join("\n>");
     else stdoutDiff += d.value;
   });
 
   document.querySelector("[stderr]").innerHTML = resp.stderr;
   document.querySelector("[stdout]").innerHTML = stdoutDiff;
+
+  if (resp.success) {
+    document.querySelector("[finish-box]").classList.remove("hidden");
+    document.querySelector("[complete-message]").innerHTML =
+      COMPLETE_MESSAGES[[Math.floor(Math.random() * COMPLETE_MESSAGES.length)]];
+    document.querySelector("[problem-badge]").style = statusBadgeColor(2);
+    document.querySelector("[problem-badge]").innerHTML = BADGE_TEXT[2];
+    return;
+  }
+
+  document.querySelector("[finish-box]").classList.remove("hidden");
+  document.querySelector("[complete-message]").innerHTML =
+    FAIL_MESSAGES[[Math.floor(Math.random() * FAIL_MESSAGES.length)]];
 }
