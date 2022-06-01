6e4a6c1c157e3f03a5f3f4780022eff2082966e3
Connor Slade <connor@connorcode.com>
2022-06-01 12:58:34 -0400

    🇿🇦 quick cpp hang fix

--- langs/c/run.sh
+++ langs/c/run.sh
@@ -1,2 +1,2 @@
 function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
-timeout --signal=KILL ${TIMEOUT} gcc -o main src/main.c && ./main $(urldecode "${ARGS}")
+timeout --signal=KILL ${TIMEOUT} gcc -o main src/main.c && timeout --signal=KILL ${TIMEOUT} ./main $(urldecode "${ARGS}")
